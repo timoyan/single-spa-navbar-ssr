2607ef55554eeb7152e72349e4303227fd432b13
Timo Yan <tomyan13@gmail.com>
2021-09-10 17:26:59 +0800

    feat: add standalone.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 const path = require("path");
 const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
+const StandaloneSingleSpaPlugin = require("standalone-single-spa-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = webpackConfigEnv => {
   const defaultConfig = singleSpaDefaults({
@@ -23,24 +25,47 @@ module.exports = webpackConfigEnv => {
     p => p.constructor.name !== "CleanWebpackPlugin"
   );
 
-  return [
-    webpackMerge.smart(defaultConfig, {}),
-    webpackMerge.smart(serverConfig, {
-      // modify the webpack config however you'd like to by adding to this object
-      target: "node",
-      mode: "development",
-      entry: path.resolve(process.cwd(), "src/node-entry.js"),
-      output: {
-        library: "mf",
-        libraryTarget: "var",
-        filename: "server.mjs"
-      },
-      externals: defaultConfig.externals.concat(/react-dom\/.+/),
-      plugins: [
-        new EsmWebpackPlugin({
-          moduleExternals: true
+  return webpackConfigEnv.standalone
+    ? [
+        webpackMerge.smart(defaultConfig, {
+          mode: webpackConfigEnv.standalone ? "development" : "production",
+          devServer: {
+            port: 8080,
+            historyApiFallback: true,
+            watchContentBase: true,
+            open: true
+          },
+          plugins: [
+            new HtmlWebpackPlugin(),
+            new StandaloneSingleSpaPlugin({
+              appOrParcelName: "@timo/navbar",
+              activeWhen: ["/navbar"],
+              HtmlWebpackPlugin
+            })
+          ]
         })
       ]
-    })
-  ];
+    : [
+        webpackMerge.smart(defaultConfig, {
+          devServer: {
+            port: 8081
+          }
+        }),
+        webpackMerge.smart(serverConfig, {
+          target: "node",
+          mode: "development",
+          entry: path.resolve(process.cwd(), "src/node-entry.js"),
+          output: {
+            library: "mf",
+            libraryTarget: "var",
+            filename: "server.mjs"
+          },
+          externals: defaultConfig.externals.concat(/react-dom\/.+/),
+          plugins: [
+            new EsmWebpackPlugin({
+              moduleExternals: true
+            })
+          ]
+        })
+      ];
 };
